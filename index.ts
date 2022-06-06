50de37528d56ec4bf8861b36b70f9d051c85d352
Grant <gergely.daniel7@gmail.com>
2022-06-06 19:50:53 +0200

    Add validation with Dvali

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/ts-node
+import validate, { arrayOf, isString } from 'dvali';
 import { constants } from 'fs';
 import { readFile, access, mkdir, writeFile } from 'fs/promises';
 import { compile } from 'handlebars';
@@ -14,7 +15,9 @@ interface BsFileWithContent {
 
 type BsFile = BsFileWithContent;
 
-interface BsParameter {}
+interface BsParameter {
+    name: string;
+}
 
 interface BsTemplate {
     name: string;
@@ -27,15 +30,32 @@ interface BsConfig {
     templates: BsTemplate[];
 }
 
+const validateBsFile = validate<BsFile>({
+    name: [isString()],
+    content: [isString()],
+});
+
+const validateBsParameter = validate<BsParameter>({
+    name: [isString()],
+});
+
+const validateBsConfig = validate<BsConfig>({
+    templates: arrayOf({
+        aliases: arrayOf([isString()]),
+        files: arrayOf(validateBsFile),
+        name: [isString()],
+        parameters: arrayOf(validateBsParameter),
+    }),
+});
+
 async function loadConfig(filename: string): Promise<BsConfig> {
     try {
-		// TODO: load from multiple files
+        // TODO: load from multiple files
         const content = await readFile(filename, 'utf8');
-		// TODO: add validation for yaml loading
-        const config = load(content) as BsConfig;
+        const config = await validateBsConfig(load(content));
         return config;
     } catch (error: any) {
-        console.error(`Loading config "${filename}" failed: ${error.getMessage()}`);
+        console.error(`Loading config "${filename}" failed: ${error}`);
         process.exit(1);
     }
 }
@@ -43,7 +63,7 @@ async function loadConfig(filename: string): Promise<BsConfig> {
 async function parseArguments() {
     return yargs(hideBin(process.argv))
         .command('create <template> <names..>', 'Generate new template', (yargs) => {
-			// TODO: add dynamic loading for template names and option parameters
+            // TODO: add dynamic loading for template names and option parameters
             return yargs
                 .positional('template', {
                     describe: 'the template to create',
@@ -91,7 +111,7 @@ function subdirs(filename: string): string[] {
 async function renderFile(file: BsFile, params: Record<string, any>): Promise<BsFile> {
     if (isFileWithContent(file)) {
         return {
-			// TODO: add helper functions
+            // TODO: add helper functions
             name: compile(file.name)(params),
             content: compile(file.content)(params),
         };
@@ -102,7 +122,7 @@ async function renderFile(file: BsFile, params: Record<string, any>): Promise<Bs
 
 async function create(template: BsTemplate, names: string[]): Promise<BsFile[]> {
     const renderedFiles = await Promise.all(
-		// TODO: add more parameters to rendering
+        // TODO: add more parameters to rendering
         names.flatMap((name) => template.files.map((f) => renderFile(f, { name })))
     );
 
@@ -119,8 +139,8 @@ async function create(template: BsTemplate, names: string[]): Promise<BsFile[]> 
         (previousPromise, dir) =>
             previousPromise.then(async () => {
                 if (!(await exists(dir))) {
-					console.log(`Directory "${dir}" created.`);
-                    mkdir(dir);
+                    console.log(`Directory "${dir}" created.`);
+                    return mkdir(dir);
                 }
             }),
         Promise.resolve()
