3448a05b27d31685eadfc06385b7a38e3e770e7d
Grant <gergely.daniel7@gmail.com>
2022-06-11 02:09:44 +0200

    Add options to create function

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -2,24 +2,26 @@ import { lstat, readFile } from 'fs/promises';
 import path from 'path';
 import { BsConfig, BsTemplate } from 'types';
 import { CREATE_OPTIONS } from '../modules/arguments';
-import { initConfig, saveConfig } from '../modules/config';
+import { findTemplate, initConfig, saveConfig } from '../modules/config';
 import { unrenderFile } from '../modules/render';
 import { exists, getFilesRecursively } from '../modules/utils';
 
 async function createTemplateFromFiles(
     templateName: string,
-    nameToReplace: string,
-    filenames: string[]
+    filenames: string[],
+    nameToReplace?: string
 ): Promise<BsTemplate> {
+    const [namespace, name] = templateName.split(':');
     return {
-        name: templateName,
-        namespace: '',
+        name,
+        namespace,
         aliases: [],
         parameters: [],
         files: await Promise.all(
             filenames.map(async (filename) => {
                 const content = await readFile(filename, 'utf-8');
-                return unrenderFile({ path: filename, content }, { name: nameToReplace });
+                const file = { path: filename, content };
+                return nameToReplace ? unrenderFile(file, { name: nameToReplace }) : file;
             })
         ),
     };
@@ -27,31 +29,58 @@ async function createTemplateFromFiles(
 
 export default async function create(
     config: BsConfig | undefined,
-    [templateName]: string[],
+    _: string[],
     params: { [key: string]: unknown }
 ) {
     if (typeof params.fromFile === 'string') {
         if (!(await exists(params.fromFile))) {
-            process.stderr.write(`File ${params.fromFile} does not exist.\n`);
-            process.exit(1);
+            throw new Error(`File ${params.fromFile} does not exist.\n`);
+        }
+
+        const templateName = params.name as string;
+        if (!templateName.includes(':')) {
+            throw new Error(
+                'Component name have to contain the namespace (e.g. react:component).\n'
+            );
+        }
+
+        const willOverwrite = config && findTemplate(config.templates, templateName);
+        if (!params.force && willOverwrite) {
+            throw new Error(
+                `Template ${templateName} already exists in config file. Add the --force option to overwrite it.\n`
+            );
         }
 
         const stat = await lstat(params.fromFile);
         let template: BsTemplate | undefined;
         const { name } = path.parse(params.fromFile);
         if (stat.isFile()) {
-            template = await createTemplateFromFiles(templateName, name, [params.fromFile]);
+            template = await createTemplateFromFiles(
+                templateName,
+                [params.fromFile],
+                !params.disableParameters ? name : undefined
+            );
         } else if (stat.isDirectory()) {
             const files = await getFilesRecursively(params.fromFile);
-            template = await createTemplateFromFiles(templateName, name, files);
+            template = await createTemplateFromFiles(
+                templateName,
+                files,
+                !params.disableParameters ? name : undefined
+            );
         } else {
             throw new Error(`Param ${params.fromFile} should be a file or a directory.\n`);
         }
 
         const updatedConfig = config ?? initConfig();
-        updatedConfig.templates.push(template);
+        updatedConfig.templates = updatedConfig.templates
+            .filter((t) => `${t.namespace}:${t.name}` !== templateName)
+            .concat(template);
         const filename = await saveConfig(updatedConfig);
-        process.stdout.write(`Saved new template ${template.name} into file ${filename}.\n`);
+        process.stdout.write(
+            `${
+                willOverwrite ? 'Overwritten' : 'Saved new'
+            } template ${templateName} in config file ${filename}.\n`
+        );
     } else {
         throw new Error(
             `You have to pass ${CREATE_OPTIONS.FROM_FILE} to read from a file or directory.\n`
--- src/modules/arguments.ts
+++ src/modules/arguments.ts
@@ -14,7 +14,11 @@ const OPTIONS = {
     VERSION: '--version',
 };
 
-export const CREATE_OPTIONS = { FROM_FILE: '--from-file' };
+export const CREATE_OPTIONS = {
+    FROM_FILE: 'from-file',
+    FORCE: 'force',
+    DISABLE_PARAMETERS: 'disable-parameters',
+};
 
 function complete(templates: BsTemplate[] = []): yargs.AsyncCompletionFunction {
     return (current, argv) => {
@@ -28,7 +32,7 @@ function complete(templates: BsTemplate[] = []): yargs.AsyncCompletionFunction {
                             (p) => `--${p.name}`
                         );
                     case COMMANDS.CREATE:
-                        return Object.values(CREATE_OPTIONS);
+                        return Object.values(CREATE_OPTIONS).map((p) => `--${p}`);
                     default:
                         return [];
                 }
@@ -119,15 +123,26 @@ export default async function parseArguments(config: BsConfig | undefined): Prom
             'Generate tab completion bash and zsh',
             complete(config?.templates)
         )
-        .command(COMMANDS.CREATE, 'Create new template', (y) =>
+        .command(`${COMMANDS.CREATE} <name>`, 'Create new template', (y) =>
             y
                 .positional('name', {
-                    describe: 'the name of the new template',
+                    describe: 'The name of the new template',
                     type: 'string',
                     demandOption: true,
                 })
                 .option(CREATE_OPTIONS.FROM_FILE, {
+                    describe: 'The name of the new template',
                     type: 'string',
+                })
+                .option(CREATE_OPTIONS.FORCE, {
+                    describe: 'Whether it should overwrite existing template',
+                    type: 'boolean',
+                    default: false,
+                })
+                .option(CREATE_OPTIONS.DISABLE_PARAMETERS, {
+                    describe: 'Whether it must not guess to replace template parts',
+                    type: 'boolean',
+                    default: false,
                 })
         )
         .completion(
