6a92ea954817bdef6ca73c8795375530ee7f2e7f
Grant <gergely.daniel7@gmail.com>
2022-09-11 15:23:49 +0200

    Add documentation to config module

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -14,18 +14,67 @@ const configurationPaths = [
     path.join(homedir(), '.bsconfig.yml'),
 ];
 
+/**
+ * Generate namespaced name from template object.
+ * This should be used internally everywhere to avoid mixups.
+ *
+ * @example
+ *      generateTemplateFullName({
+ *          namespace: 'react',
+ *          name: 'component',
+ *          ...
+ *      }); // => 'react:component'
+ *
+ * @param template the template object
+ * @returns the namespaced name
+ */
 export function generateTemplateFullname(template: BsTemplate): string {
     return `${template.namespace}:${template.name}`;
 }
 
+/**
+ * Generate all different version for a template name (name, fullname + aliases).
+ * Primarily used for completion.
+ *
+ * @example
+ *      generateTemplateNames({
+ *          namespace: 'react',
+ *          name: 'component',
+ *          aliases: ['rc'],
+ *          ...
+ *      }); // => 'react:component'
+ *
+ * @param template the template object
+ * @returns an array of names
+ */
 export function generateTemplateNames(template?: BsTemplate): string[] {
     return template ? [generateTemplateFullname(template), template.name, ...template.aliases] : [];
 }
 
+/**
+ * Generate list of parameter names for a template. Primarily used for completion.
+ *
+ * @param template the template object
+ * @returns the list of the parameter names
+ */
 export function generateTemplateParamNames(template?: BsTemplate): string[] {
     return template?.parameters?.map((p) => p.name) ?? [];
 }
 
+/**
+ * Find a template in the given list of templates by name, fullname or alias.
+ *
+ * @example
+ *      const template = { name: 'component', namespace: 'react', aliases: 'rc', ... };
+ *      findTemplate([template], 'component'); => // template
+ *      findTemplate([template], 'react:component'); => // template
+ *      findTemplate([template], 'rc'); => // template
+ *      findTemplate([template], 'asdasd'); => // undefined
+ *
+ * @param templates the list of templates to search in
+ * @param name the name, fullname or alias of the template
+ * @returns the first matching template object or undefined if none found
+ */
 export function findTemplate(templates: BsTemplate[], name: string): BsTemplate | undefined {
     return (
         templates.find((t) => generateTemplateFullname(t) === name) ??
@@ -34,6 +83,19 @@ export function findTemplate(templates: BsTemplate[], name: string): BsTemplate 
     );
 }
 
+/**
+ * Find the templates, based on a list of names. Primarily used for includes.
+ *
+ * @example
+ *      const template = { name: 'component', namespace: 'react', ... };
+ *      findReferences([template], ['component']); => // [template]
+ *      findTemplate([template], ['asdasd']); => // throw new Error(...)
+ *
+ * @param templates the list of templates to search in
+ * @param includes the list of names, fullnames or aliases of the templates (mostly aliases)
+ * @returns the list of found templates
+ * @throws if one of the templates not found
+ */
 export function findReferences(templates: BsTemplate[], includes: string[]): BsTemplate[] {
     const referencedTemplates: [string, BsTemplate | undefined][] = includes.map((t) => [
         t,
@@ -50,6 +112,15 @@ export function findReferences(templates: BsTemplate[], includes: string[]): BsT
     return referencedTemplates.map(([, t]) => t!);
 }
 
+/**
+ * Recursively resolve the included templates.
+ * Also collects the templates that are already included, to avoid infinite include loops.
+ *
+ * @param templates the list of templates to search in
+ * @param template the template to start the includes from
+ * @param alreadyIncluded the templates that are already included
+ * @returns the list of included templates recursively, or a one-item list if files templates
+ */
 export function getReferencedFileTemplates(
     templates: BsTemplate[],
     template: BsTemplate,
@@ -73,21 +144,44 @@ export function getReferencedFileTemplates(
     );
 }
 
+/**
+ * Get a deduplicated list of files from list of files templates.
+ *
+ * @param templates the list of files templates
+ * @returns the list of files to render
+ */
 export function getFilesFromTemplates(templates: BsFilesTemplate[]): BsFile[] {
     return uniqBy((f) => f.path, templates.flatMap((t) => t.files).reverse());
 }
 
+/**
+ * Create a minimal new configuration.
+ *
+ * @returns the configuration object
+ */
 export function initConfig(): BsConfig {
     return {
         templates: [],
     };
 }
 
+/**
+ * Find a configuration file in the current directory or in any of the parent directories
+ *
+ * @returns the name of the config file or undefined if not found
+ */
 export async function getConfigFile(): Promise<string | undefined> {
     const filename = await findUp(configurationPaths);
     return filename;
 }
 
+/**
+ * Load the config from the current directory, or any of the parent directories
+ * and parses the yaml to a config object.
+ *
+ * @returns the config object or undefined if not found
+ * @throws error if config loading failed
+ */
 export async function loadConfig(): Promise<BsConfig | undefined> {
     const filename = await getConfigFile();
     if (!filename) {
@@ -102,6 +196,12 @@ export async function loadConfig(): Promise<BsConfig | undefined> {
     }
 }
 
+/**
+ * Dumps the config object to yaml and saves it to the default config path.
+ *
+ * @param config the config object
+ * @returns the filename the config has been saved
+ */
 export async function saveConfig(config: BsConfig): Promise<string> {
     const filename = (await getConfigFile()) ?? configurationPaths[0];
     try {
