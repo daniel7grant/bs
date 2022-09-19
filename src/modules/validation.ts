9239b08c2c6f25a8728d744f9a022aff96f6b16f
Grant <gergely.daniel7@gmail.com>
2022-09-20 00:16:42 +0200

    Fix validation with Dvali update

--- src/modules/validation.ts
+++ src/modules/validation.ts
@@ -1,12 +1,12 @@
-import validate, { arrayOf, isBool, isString, optional } from 'dvali';
-import { BsConfig, BsFile, BsParameter } from '../types.js';
+import validate, { arrayOf, either, isBool, isString, optional } from 'dvali';
+import { BsConfig } from '../types.js';
 
-const validateBsFile = validate<BsFile>({
+const validateBsFile = validate({
     path: [isString()],
     content: [isString()],
 });
 
-const validateBsParameter = validate<BsParameter>({
+const validateBsParameter = validate({
     name: [isString()],
     description: optional([isString()]),
     type: [isString()],
@@ -14,15 +14,24 @@ const validateBsParameter = validate<BsParameter>({
     required: [isBool()],
 });
 
+const validateBaseBsTemplate = {
+    name: [isString()],
+    namespace: [isString()],
+    description: optional([isString()]),
+    aliases: arrayOf([isString()]),
+    parameters: arrayOf(validateBsParameter),
+};
+
+const validateBsFilesTemplate = validate({
+    ...validateBaseBsTemplate,
+    files: arrayOf(validateBsFile),
+});
+
+const validateBsIncludeTemplate = validate({
+    ...validateBaseBsTemplate,
+    includes: arrayOf(isString()),
+});
+
 export default validate<BsConfig>({
-    templates: arrayOf({
-        name: [isString()],
-        namespace: [isString()],
-        description: optional([isString()]),
-        aliases: arrayOf([isString()]),
-        // TODO: fix validation
-        files: arrayOf(validateBsFile),
-        includes: optional(arrayOf(isString())),
-        parameters: optional(arrayOf(validateBsParameter)),
-    }),
+    templates: arrayOf(either(validateBsFilesTemplate, validateBsIncludeTemplate)),
 });
